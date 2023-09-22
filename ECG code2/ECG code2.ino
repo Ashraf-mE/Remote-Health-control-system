e2473f63e1479c9575d0f63f8dc63a252d9c6d4b
Mohammad Ashraf P <mohammadashrafp10@gmail.com>
2023-09-23 02:25:18 +0530

    Update ECG code2.ino

--- ECG code2/ECG code2.ino
+++ ECG code2/ECG code2.ino
@@ -5,7 +5,7 @@
 #define FIREBASE_HOST "remote-health-monitor-b1a62-default-rtdb.firebaseio.com" //https://remote-health-monitor-b1a62-default-rtdb.firebaseio.com/
 #define WIFISSID "ashraf754"                         // Put your WifiSSID here
 #define PASSWORD "ashraf123"                         // Put your wifi password here
-#define TOKEN "BBFF-cyPGOc5hADPAeRt5VvDXd96doGQpI8"  // Ashraf API code: "BBFF-Fa5BhZKZ0wnRUbCxBKrJm9WMSt3sqD"         // Put your Ubidots' TOKEN
+#define TOKEN {UBIDOTS-Token}   // Ashraf API code: {UBIDOTS-Code}        // Put your Ubidots' TOKEN
 #define MQTT_CLIENT_NAME "myecgsensor"               // MQTT client Name, please enter your own 8-12 alphanumeric character ASCII string;
 #define FIREBASE_Authorization_key "9qlV5sXOfxTETB2dJuAuhS34yD0UlWFKKpjJJxtS"
 FirebaseData firebaseData;
